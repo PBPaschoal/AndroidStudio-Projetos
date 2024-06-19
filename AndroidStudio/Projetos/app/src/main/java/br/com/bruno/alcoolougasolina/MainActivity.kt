3c9fde8757e6707e8e0c319c7689466cc9914844
Paulo Bruno Paschoal <programming.paschoal@gmail.com>
2024-06-18 21:12:29 -0300

    Desenvolvendo meu primeiro aplicativo no AndroidStudio

---
+++ AndroidStudio/Projetos/app/src/main/java/br/com/bruno/alcoolougasolina/MainActivity.kt
@@ -0,0 +1,43 @@
+package br.com.bruno.alcoolougasolina
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.bruno.alcoolougasolina.ui.theme.AlcoolOuGasolinaTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AlcoolOuGasolinaTheme {
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )*/
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(){
+
+}
